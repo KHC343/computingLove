86da2f116a59bc223771f04425a1bdc3c4edd415
SRMorris <morrisherd@gmail.com>
2015-09-15 12:15:01 -0500

    Have a working pong game. Can't control it and the physics are weird, but I'll take it.

--- ComputerLoving/PongViewController.swift
+++ ComputerLoving/PongViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class PongViewController: UIViewController {
+class PongViewController: UIViewController, UICollisionBehaviorDelegate {
     
+    var allViews = [UIView]()
     var collisonBehavior = UICollisionBehavior()
     var playerPaddle : UIView = UIView()
     var enemyPaddle : UIView = UIView()
@@ -26,18 +27,21 @@ class PongViewController: UIViewController {
         playerPaddle.backgroundColor = UIColor.blueColor()
         playerPaddle.layer.cornerRadius = 5
         playerPaddle.layer.masksToBounds = true
+        allViews.append(playerPaddle)
         view.addSubview(playerPaddle)
         
         enemyPaddle = UIView(frame: CGRectMake(150, view.center.y, 30, 90))
         enemyPaddle.backgroundColor = UIColor.redColor()
         enemyPaddle.layer.cornerRadius = 5
         enemyPaddle.layer.masksToBounds = true
+        allViews.append(enemyPaddle)
         view.addSubview(enemyPaddle)
         
         ball = UIView(frame: CGRectMake(view.center.x, view.center.y, 20, 20))
         ball.backgroundColor = UIColor.greenColor()
         ball.layer.cornerRadius = 10
         ball.layer.masksToBounds = true
+        allViews.append(ball)
         view.addSubview(ball)
         
         ballBehavior = UIDynamicItemBehavior(items: [ball])
